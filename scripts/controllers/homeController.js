d9def92dc2b15f9a44e070c52550d1c4fe486b44
nagesh313 <nagesh3.13@gmail.com>
2017-05-19 19:52:02 +0530

    Working Home Controller checkin

--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -1,7 +1,9 @@
 'use strict';
-angular.module('weatherApp')
+angular.module('weatherForecastApp')
   .controller('HomeController', ['WeatherService', '$scope', function (WeatherService, $scope) {
+
     $scope.list = WeatherService.getForecastForDayByCurrentLocation().then(function (data) {
       $scope.list = data;
     }, function () { });
+
   }]);
